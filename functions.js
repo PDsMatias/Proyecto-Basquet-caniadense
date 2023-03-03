a74149c8aa6596e03e514e039d9f647745364359
PDsMatias <maticanalla1979@gmail.com>
2023-03-03 17:54:08 -0300

    a

--- functions.js
+++ functions.js
@@ -54,3 +54,54 @@ function a(b,c) {
 function onclickU16() {
   alert("holaaaaa")
 }
+/**
+       * Print the names and majors of students in a sample spreadsheet:
+       * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+       */
+async function listMajors() {
+    let response;
+    console.log("Hello world!")
+    try {
+      // Fetch first 10 files
+      response = await gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1AioXlg1vKJvTMiOdZpnSYAM3YFg8qBhQH_136hqwLBk',
+        range: 'Hoja 1!A1:B2',
+      });
+    } catch (err) {
+      document.getElementById('content').innerText = err.message;
+      return;
+    }
+    const range = response.result;
+    if (!range || !range.values || range.values.length == 0) {
+      document.getElementById('content').innerText = 'No values found.';
+      return;
+    }
+    // Flatten to string to display
+    const output = range.values.reduce(
+        (str, row) => `${str}${row[0]}, ${row[4]}\n`,
+        'Name, Major:\n');
+    document.getElementById('content').innerText = output;
+  }
+  function gapiLoaded() {
+    gapi.load('client', initializeGapiClient);
+  }
+
+  /**
+   * Callback after the API client is loaded. Loads the
+   * discovery doc to initialize the API.
+   */
+  async function initializeGapiClient() {
+    await gapi.client.init({
+      apiKey: API_KEY,
+      discoveryDocs: [DISCOVERY_DOC],
+    });
+  }
+      const CLIENT_ID = '<YOUR_CLIENT_ID>';
+      const API_KEY = 'AIzaSyCAOgMfg-52HO8cqWt0J65qc_87tTZhojE';
+
+      // Discovery doc URL for APIs used by the quickstart
+      const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+      const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
